fa86d3a9c2cdcc7f0a69eb42eeae0708e81d22d7
Tanvee Srivastava <48172946+tanveesrivastava@users.noreply.github.com>
2021-05-25 20:15:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,60 @@
+import React , {useState,useEffect} from 'react';
+import "./App.css";
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
+function App(){
+
+    const [inputText,setInputText]=useState("")
+    const[todos,setTodos]=useState([])
+    const [status,setStatus]=useState("all")
+    const [filteredTodos,setFilteredTodos]=useState([])
+    
+
+    //use effect
+    useEffect(()=>{
+        filterHandler( )
+    },[todos,status])
+
+const filterHandler=()=>{
+    switch(status){
+        case 'completed':
+            setFilteredTodos(todos.filter(todo=>todo.completed===true))
+            break;
+        case 'uncompleted':
+            setFilteredTodos(todos.filter(todo=>todo.completed===false))
+            break;
+        default:
+            setFilteredTodos(todos);
+            break;
+    }
+}
+    return(
+        <>
+        <div className="App">
+        <header>
+
+            <h1>What Needs to be done</h1>
+            
+        </header>
+        <Form 
+        inputText={inputText} 
+        todos={todos} 
+        setTodos={setTodos} 
+        setInputText={setInputText}
+        setStatus={setStatus}
+        
+        />
+
+        <TodoList 
+        setTodos={setTodos} 
+        todos={todos}
+        filteredTodos={filteredTodos}
+        
+        />
+        </div>
+        </>
+    )
+}
+
+export default App 
